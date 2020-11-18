750f6b1b08b572b5cad923facbcc66daf7ccb6b7
PhysicalRevelry <kristinnewmancscs@gmail.com>
2020-11-18 11:40:27 -0600

    Matt helped me interpret docs around TweenAnimationBuilder
    None of the tutorials I looked at explained each piece of the code and I was "wreck and check" experimenting to figure it out.

--- lib/main.dart
+++ lib/main.dart
@@ -27,131 +27,147 @@ class _EmotionsPageState extends State<EmotionsPage> with SingleTickerProviderSt
   String feedBackText = "Very Poor";
   Color backgroundColor = Colors.red;
   String howFeeling = "Very Poor";
+  // Color endingColor;
+  // Color startingColor = Colors.red;
 
+  ///variables for the tween usage
+  Color _lastColor = Colors.red;
+  Color _nextColor = Colors.red;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: backgroundColor,
-        body: Stack(
-          children: <Widget> [
-            Center(
-              child: FlareActor(
-                'assets/flares/feelings.flr',
-                fit: BoxFit.contain,
-                alignment: Alignment.center,
-                animation: feel,
-              ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 60),
-                  child: Text(howFeeling,
-                  style: TextStyle(
-                    fontSize: 36,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),),
+      ///TWeen Animation Builder
+      child: TweenAnimationBuilder(
+        tween: ColorTween(begin: _lastColor, end: _nextColor), ///Start and end colors
+        duration: Duration(milliseconds: 500),
+        builder: (BuildContext context, Color color, _){
+          return Scaffold(
+            backgroundColor: color,
+            body: Stack(
+              children: <Widget> [
+                Center(
+                  child: FlareActor(
+                    'assets/flares/feelings.flr',
+                    fit: BoxFit.contain,
+                    alignment: Alignment.center,
+                    animation: feel,
+                  ),
                 ),
-                Slider(
-                  value: _value,
-                  min: 0.0,
-                  max: 100.0,
-                  divisions: 100,
-                  activeColor: Colors.blue,
-                  label: "How satisfied are you?",
-                  onChanged: (val){
-                    ///Feeling really bad, slider at 0
-                    setState(() {
-                      _value = val;
-                    });
-                    if(_value == 0.0){
-                      if(lastSection > 0.0){
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 60),
+                      child: Text(howFeeling,
+                        style: TextStyle(
+                          fontSize: 36,
+                          color: color,
+                          fontWeight: FontWeight.bold,
+                        ),),
+                    ),
+                    Slider(
+                      value: _value,
+                      min: 0.0,
+                      max: 100.0,
+                      divisions: 100,
+                      activeColor: Colors.blue,
+                      label: "How satisfied are you?",
+                      onChanged: (val){
+                        ///Feeling really bad, slider at 0
                         setState(() {
-                          feel = "0-";
+                          _value = val;
                         });
-                      }
-                      setState(() {
-                        lastSection = 0.0;
-                        backgroundColor = Colors.red;
-                        howFeeling = "Very Poor";
-                      });
-                    }
-                    ///Feeling less bad, slider between 0-25
-                    else if (_value > 0.0 && _value < 25.0){
-                      if(lastSection == 0.0){
-                        setState(() {
-                          feel = "0+";
-                        });
-                      } else if (lastSection == 50.0){
-                        setState(() {
-                          feel = "25-";
-                        });
-                      }
-                      setState(() {
-                        lastSection = 25.0;
-                        backgroundColor = Colors.orange;
-                        howFeeling = "Poor";
-                      });
-                    }
-                    /// feeling neutral, slider between 25-50
-                    else if (_value >= 25.0 && _value < 50.0){
-                      if(lastSection == 25.0){
-                        setState(() {
-                          feel = "25+";
-                        });
-                      } else if (lastSection == 75.0){
-                        setState(() {
-                          feel = "50-";
-                        });
-                      }
-                      setState(() {
-                        lastSection = 50.0;
-                        backgroundColor = Colors.yellow;
-                        howFeeling = "Okay";
-                      });
-                    }
-                    ///Feleing okay, slider between 50-75
-                    else if (_value >= 50.0 && _value < 75.0){
-                      if (lastSection == 50.0){
-                        setState(() {
-                          feel = "50+";
-                        });
-                      } else if (lastSection == 100 ){
-                        setState(() {
-                          feel = "75-";
-                        });
-                      }
-                      setState(() {
-                        lastSection = 75.0;
-                        backgroundColor = Colors.lightGreen;
-                        howFeeling = "Good";
-                      });
-                    }
-                    ///Feeling pretty good, slider between 75-100
-                    else if(_value >= 75.0 && _value <=100){
-                      if(lastSection == 75){
-                        setState(() {
-                          feel = "75+";
-                        });
-                      }
-                      setState(() {
-                        lastSection = 100;
-                        backgroundColor = Colors.green;
-                        howFeeling = "Excellent";
-                      });
-                    }
-                  },
-                ),
+                        if(_value == 0.0){
+                          if(lastSection > 0.0){
+                            setState(() {
+                              feel = "0-";
+                            });
+                          }
+                          setState(() {
+                            lastSection = 0.0;
+                            _lastColor = color;
+                            _nextColor = Colors.red;
+                            howFeeling = "Very Poor";
+                          });
+                        }
+                        ///Feeling less bad, slider between 0-25
+                        else if (_value > 0.0 && _value < 25.0){
+                          if(lastSection == 0.0){
+                            setState(() {
+                              feel = "0+";
+                            });
+                          } else if (lastSection == 50.0){
+                            setState(() {
+                              feel = "25-";
+                            });
+                          }
+                          setState(() {
+                            lastSection = 25.0;
+                            _lastColor = color;
+                            _nextColor = Colors.orange;
+                            howFeeling = "Poor";
+                          });
+                        }
+                        /// feeling neutral, slider between 25-50
+                        else if (_value >= 25.0 && _value < 50.0){
+                          if(lastSection == 25.0){
+                            setState(() {
+                              feel = "25+";
+                            });
+                          } else if (lastSection == 75.0){
+                            setState(() {
+                              feel = "50-";
+                            });
+                          }
+                          setState(() {
+                            lastSection = 50.0;
+                            _lastColor = color;
+                            _nextColor = Colors.yellow;
+                            howFeeling = "Okay";
+                          });
+                        }
+                        ///Feleing okay, slider between 50-75
+                        else if (_value >= 50.0 && _value < 75.0){
+                          if (lastSection == 50.0){
+                            setState(() {
+                              feel = "50+";
+                            });
+                          } else if (lastSection == 100 ){
+                            setState(() {
+                              feel = "75-";
+                            });
+                          }
+                          setState(() {
+                            lastSection = 75.0;
+                            _lastColor = color;
+                            _nextColor = Colors.lightGreen;
+                            howFeeling = "Good";
+                          });
+                        }
+                        ///Feeling pretty good, slider between 75-100
+                        else if(_value >= 75.0 && _value <=100){
+                          if(lastSection == 75){
+                            setState(() {
+                              feel = "75+";
+                            });
+                          }
+                          setState(() {
+                            lastSection = 100;
+                            _lastColor = color;
+                            _nextColor = Colors.green;
+                            howFeeling = "Excellent";
+                          });
+                        }
+                      },
+                    ),
+                  ],
+                )
               ],
-            )
-          ],
-        ),
-
+            ),
+          );
+        }
       ),
     );
   }
